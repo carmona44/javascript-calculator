71195b13fa63dc277308c954ccb948e4146f6524
carmona44 <daniel_95ca@hotmail.com>
2019-01-20 00:39:39 +0100

    Updates

---
+++ precache-manifest.0e5a6c7ea2919d9adcc5c1cddc9ab530.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc7773250f7c400c052",
+    "url": "/javascript-calculator/static/js/runtime~main.9bc77732.js"
+  },
+  {
+    "revision": "cdfd09499399ee9794e3",
+    "url": "/javascript-calculator/static/js/main.cdfd0949.chunk.js"
+  },
+  {
+    "revision": "1d714d2257181748a59b",
+    "url": "/javascript-calculator/static/js/1.1d714d22.chunk.js"
+  },
+  {
+    "revision": "cdfd09499399ee9794e3",
+    "url": "/javascript-calculator/static/css/main.b399ad9d.chunk.css"
+  },
+  {
+    "revision": "0230a22a6785b4625e8c10a91ebd3069",
+    "url": "/javascript-calculator/index.html"
+  }
+];
